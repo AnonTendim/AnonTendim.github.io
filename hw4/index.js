f58ea0678bdba6999647dcbdfc49b806b69fd944
xh2318@columbia.com <AnonTendim>
2019-04-05 16:20:59 +0000

    new model

--- hw4/index.js
+++ hw4/index.js
@@ -8,13 +8,13 @@ const HOSTED_URLS = {
 
 const examples = {
   'example1':
-      'Alice was beginning to get very tired of sitting by her sister on the bank.',
+      'Lastly, she pictured to herself how this same little sister of hers would, in the after-time, be herself a grown woman.',
   'example2':
-      'Buda-Pesth seems a wonderful place.',
+      'The wind came now in fierce bursts, and the snow was driven with fury as it swept upon us in circling eddies.',
   'example3':
-      'The time employed in this book consists not entirely of one day.',
+      'The Trojans fly before Achilles, some towards the town, others to the river Scamander; he falls upon the latter with great slaughter, takes twelve captives alive, to sacrifice to the shade of Patroclus.',
   'example4':
-      'It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.'
+      'But to live in ignorance on such a point was impossible; or at least it was impossible not to try for information.'
 };
 
 function status(statusText) {
